39e60c3488523d93c7f616eddd85045b8a18a836
Jessica <jessicalaporte888@gmail.com>
2022-06-21 15:06:44 -0700

    modals and education

--- index.js
+++ index.js
@@ -24,3 +24,42 @@ btns.forEach(function(eachBtn){
         onButtonFilterClick(eachBtn);
     })
 })
+
+
+// Modals
+
+const modalBtns = document.querySelectorAll('[data-modal]');
+const overlayModal = document.getElementById('modal-overlay');
+
+modalBtns.forEach(function(eachModalBtn){
+
+    eachModalBtn.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const modalID = eachModalBtn.dataset.modal;
+        const modal = document.getElementById(modalID);
+
+        modal.classList.toggle('closed');
+        overlayModal.classList.remove('closed');
+        document.body.classList.add('overflow');
+    })
+
+})
+
+const closeModalBtns = document.querySelectorAll('.modal .close-button');
+
+closeModalBtns.forEach(function(eachCloseModalBtn){
+    eachCloseModalBtn.addEventListener('click', function(){
+        eachCloseModalBtn.parentNode.classList.toggle('closed');
+        overlayModal.classList.add('closed');
+        document.body.classList.remove('overflow');
+    })
+})
+
+// Close if click outside (on the overlay)
+overlayModal.addEventListener('click', function(){
+    const openModal = document.querySelector('.modal:not(.closed)')
+    overlayModal.classList.add('closed');
+    document.body.classList.remove('overflow');
+    openModal.classList.add('closed');
+})
